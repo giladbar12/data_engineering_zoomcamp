a6b492bb522a7c39a43549efe58fd563782bc3c8
giladbar12 <38021452+giladbar12@users.noreply.github.com>
2026-07-16 21:28:03 +0000

    changed code to ingest the data for the homework questions

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,37 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import pandas as pd 
+import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 import click
 
 
-
-dtype = {
-    "VendorID": "Int64",
-    "passenger_count": "Int64",
-    "trip_distance": "float64",
-    "RatecodeID": "Int64",
-    "store_and_fwd_flag": "string",
-    "PULocationID": "Int64",
-    "DOLocationID": "Int64",
-    "payment_type": "Int64",
-    "fare_amount": "float64",
-    "extra": "float64",
-    "mta_tax": "float64",
-    "tip_amount": "float64",
-    "tolls_amount": "float64",
-    "improvement_surcharge": "float64",
-    "total_amount": "float64",
-    "congestion_surcharge": "float64"
+SOURCES = {
+    "green_taxi_data_Nov_2025": {
+        "url": "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-11.parquet",
+        "kind": "parquet",
+    },
+    "taxi_zone_lookup": {
+        "url": "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv",
+        "kind": "csv",
+    },
 }
 
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
 
+def parquet_chunks(url, chunksize):
+    df = pd.read_parquet(url)
+    for start in range(0, len(df), chunksize):
+        yield df.iloc[start:start + chunksize]
 
 
 @click.command()
@@ -40,52 +31,45 @@ parse_dates = [
 @click.option('--pg-host', default='localhost', help='PostgreSQL host')
 @click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
 @click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
-@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
-def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
-    
-    year = 2021 
-    month = 1
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_database = 'ny_taxi'
-    chunksize = 100000
-    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-    url = f'{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz'
-    target_table = "yellow_taxi_data"
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading source data')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, chunksize):
 
-    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_database}')
-    df_iter = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator=True,
-    chunksize=chunksize
-    )
+    engine = create_engine(f'postgresql+psycopg2://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
-    first = True
+    for target_table, source in SOURCES.items():
+        print(f"Loading {target_table} from {source['url']}")
 
-    for df_chunk in tqdm(df_iter):
+        if source["kind"] == "parquet":
+            df_iter = parquet_chunks(source["url"], chunksize)
+        else:
+            df_iter = pd.read_csv(
+                source["url"],
+                iterator=True,
+                chunksize=chunksize
+            )
 
-        if first:
-            # Create table schema (no data)
-            df_chunk.head(0).to_sql(
+        first = True
+
+        for df_chunk in tqdm(df_iter):
+
+            if first:
+                # Create table schema (no data)
+                df_chunk.head(0).to_sql(
+                    name=target_table,
+                    con=engine,
+                    if_exists="replace"
+                )
+                first = False
+                print("Table created:", target_table)
+
+            # Insert chunk
+            df_chunk.to_sql(
                 name=target_table,
                 con=engine,
-                if_exists="replace"
+                if_exists="append"
             )
-            first = False
-            print("Table created")
 
-        # Insert chunk
-        df_chunk.to_sql(
-            name=target_table,
-            con=engine,
-            if_exists="append"
-        )
-
-        print("Inserted:", len(df_chunk))
+            print("Inserted:", len(df_chunk))
 
 if __name__ == "__main__":
-    run()  
+    run()
